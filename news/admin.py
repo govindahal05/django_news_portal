d9db469b48f24d205d5b69fa3aeb33b308485ecf
Govinda Dahal <govindahal05@gmail.com>
2020-01-26 15:51:05 +0545

    git module updated - comment, edit, delete functionality added

--- news/admin.py
+++ news/admin.py
@@ -3,13 +3,11 @@ from news.models import News, Category
 
 # Register your models here.
 
-
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "created_at")
     prepopulated_fields = {"slug": ("title",)}
 
     # readonly_fields = ("author",)
-
 
 admin.site.register(Category)
--- news/forms.py
+++ news/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from news.models import News, Category
 
+
 class NewsCreateForm(forms.ModelForm):
     CATEGORY_CHOICES = [(category.id, category.title) for category in Category.objects.all()]
-    # CATEGORY_CHOICES = [(1, 'One'),(2, 'One'),(3, 'One')]
     category = forms.MultipleChoiceField(
         required=True, widget=forms.CheckboxSelectMultiple, choices=CATEGORY_CHOICES,
     )
