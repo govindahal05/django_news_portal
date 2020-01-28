372b29ecda44763bc0f34a7e892fdea4625d1ad6
Govinda Dahal <govindahal05@gmail.com>
2020-01-28 13:06:00 +0545

    news module updated

--- news/forms.py
+++ news/forms.py
@@ -3,6 +3,7 @@ from news.models import News, Category
 
 
 class NewsCreateForm(forms.ModelForm):
+    # CATEGORY_CHOICES = [("0", "title 1"), ("1", "title 2"), ("2", "title 3")]
     CATEGORY_CHOICES = [(category.id, category.title) for category in Category.objects.all()]
     category = forms.MultipleChoiceField(
         required=True, widget=forms.CheckboxSelectMultiple, choices=CATEGORY_CHOICES,
